504b8c7dbf54b35d3492a377b15ae614b46d804d
Gabriel Scopel <gabriel.souza.scopel@hotmail.com>
2022-04-18 13:13:02 -0300

    updata 18/04

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 const admin = require("./routes/admin") //referenciando nossa routes e atribuindo a constante
 const path = require("path") //usando o path, que nos auxilia a manipular pastas no node
 //const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 //public
 app.use(express.static(path.join(__dirname,"public")))//anunciando que os arquivos estáticos estão na pasta public
@@ -18,6 +19,12 @@ app.use(express.static(path.join(__dirname,"public")))//anunciando que os arquiv
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
     //mongoose
+    mongoose.Promise = global.Promise;
+    mongoose.connect("mongodb://localhost/blogapp").then(()=> {
+        console.log("Conectado ao mongo.")
+    }).catch((err)=>{
+        console.log("Erro ao se conectar: "+err)
+    })
 // Rotas
     app.use("/admin", admin) //referenciando as rotas da pasta routes que foram armazenadas na constante
 // Outros
