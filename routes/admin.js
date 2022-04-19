638125f757ebe963da5121ebc76c03a154e2ba06
Gabriel Scopel <gabriel.souza.scopel@hotmail.com>
2022-04-19 00:01:10 -0300

    update

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router() // componente que nos possibilita usar rotas em arquivos separados
-
-
+const mongoose = require("mongoose")
+require("../models/Categoria")
+const Categoria = mongoose.model("categorias")
 router.get("/",(req, res)=>{   //criando uma rota do tipo get
     res.render("admin/index")
 })
@@ -12,6 +13,18 @@ router.get("/posts",(req, res)=>{
 
 router.get("/categorias",(req, res)=>{
     res.render("admin/categorias")
+})
+
+router.post("/categorias/nova", (req,res)=>{
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+    new Categoria(novaCategoria).save().then(()=>{
+    console.log("Categoria salva com sucesso.")
+}).catch((err)=>{
+    console.log("Erro ao salvar a categoria")
+    })
 })
 
 router.get("/categorias/add",(req, res)=>{
